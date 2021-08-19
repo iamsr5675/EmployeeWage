f29b53c568f2d0d7268bbc0bc8ac1b68091b780d
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-20 00:07:08 +0530

    Employee attendance check

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,6 +4,16 @@ public class EmployeeWage
 {
 	public static void main(String args[])
     {
-    System.out.println("Welcome to Employee Wage computation Problem");    
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
     }
 }
