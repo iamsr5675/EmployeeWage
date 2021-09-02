523fd56d1efe5f8d56a636175c73d4b7fea8df4b
iamsr5675 <saurabhranjan5675@gmail.com>
2021-09-02 21:25:49 +0530

    for multiple companies

--- src/main/java/com/bridgelabz/FellowshipProgram/EmployeeWage.java
+++ src/main/java/com/bridgelabz/FellowshipProgram/EmployeeWage.java
@@ -1,26 +1,24 @@
 package com.bridgelabz.FellowshipProgram;
 
 public class EmployeeWage {
-	void EmployeeWage1() {
-		
-		int empWagePerHour=50;
-		int WorkingHoursPerDay=8;
-		int WoringDaysInMonth=20;
-		int MonthlySalary=  empWagePerHour*WorkingHoursPerDay*WoringDaysInMonth;
-		System.out.println("Monthly salary of employee of comapany A is "+MonthlySalary);
-	}	
-	void EmployeeWage2() {
-		int empWagePerHour=20;
-		int WorkingHoursPerDay=5;
-		int WoringDaysInMonth=24;
-		int MonthlySalary=  empWagePerHour*WorkingHoursPerDay*WoringDaysInMonth;
-		System.out.println("Monthly salary of employee of comapany B is "+MonthlySalary);	
-		}
-	public static void main(String[] args ) {
-		   System.out.println("Welcome to Employee Wage Computation");
-		   EmployeeWage r =new EmployeeWage();
-		   r.EmployeeWage1();
-		   r.EmployeeWage2();
-		   }
-
-}
+		void EmployeeWage1() {	
+			int empWagePerHour=50;
+			int WorkingHoursPerDay=8;
+			int WoringDaysInMonth=20;
+			int MonthlySalary=  empWagePerHour*WorkingHoursPerDay*WoringDaysInMonth;
+			System.out.println("Monthly salary of employee of comapany A is "+MonthlySalary);
+		}	
+		void EmployeeWage2() {
+			int empWagePerHour=20;
+			int WorkingHoursPerDay=5;
+			int WoringDaysInMonth=24;
+			int MonthlySalary=  empWagePerHour*WorkingHoursPerDay*WoringDaysInMonth;
+			System.out.println("Monthly salary of employee of comapany B is "+MonthlySalary);	
+			}
+		public static void main(String[] args ) {
+			   System.out.println("Welcome to Employee Wage Computation");
+			   EmployeeWage r =new EmployeeWage();
+			   r.EmployeeWage1();
+			   r.EmployeeWage2();
+			   }
+	}
